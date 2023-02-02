067a8f8203efba2e570e557afb020a7a9cb70bf4
Anna Tereshko <anna.tereshko@materna.de>
2023-02-02 12:20:18 +0100

    no message

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+let products = document.getElementsByClassName("product");
+let productTitle = document.getElementsByClassName("product__title");
+
+function test(){
+
+    if(document.getElementById("checkbox").checked){
+        console.log("########");
+        for(i=0; i < products.length; i++){
+            products[i].style.zIndex = "-1";
+            productTitle[i].style.zIndex = "-1";
+        }
+    }else{
+        for(i=0; i < products.length; i++){
+            products[i].style.zIndex = "0";
+            productTitle[i].style.zIndex = "0";
+        }
+    }
+}
+
+document.querySelector('#checkbox').addEventListener('click', test);
