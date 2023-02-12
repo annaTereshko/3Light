781d77752c80b339ecf4c6c8fd0d2cb53ff87dcb
Tereshko <anna.tereshko@materna.de>
2023-02-12 20:36:09 +0100

    no message

--- js/main.js
+++ js/main.js
@@ -1,20 +1,29 @@
 
-let products = document.getElementsByClassName("product");
-let productTitle = document.getElementsByClassName("product__title");
+function changeZIndex(){
 
-function test(){
+    let products = document.getElementsByClassName("product");
+    let productTitle = document.getElementsByClassName("product__title");
+    let navigation = document.getElementById("navigation");
+    let policyLink = document.getElementById("policy");
 
     if(document.getElementById("checkbox").checked){
         for(i=0; i < products.length; i++){
             products[i].style.zIndex = "-1";
             productTitle[i].style.zIndex = "-1";
         }
+        navigation.style.transform = "none";
+        policyLink.style.zIndex = "-1";
+
     }else{
         for(i=0; i < products.length; i++){
             products[i].style.zIndex = "0";
             productTitle[i].style.zIndex = "0";
         }
+        navigation.style.transform = "translate(-150%, 0%)";
+        policyLink.style.zIndex = "0";
     }
 }
 
-document.querySelector('#checkbox').addEventListener('click', test);
+
+
+document.querySelector('#checkbox').addEventListener('click', changeZIndex);
