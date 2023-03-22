c494713b5a77f4b7ff7bf51f80b7d2917be94f8e
atereshk <anna.tereshko@materna.de>
2023-03-22 10:37:23 +0100

    no message

--- js/main.js
+++ js/main.js
@@ -24,6 +24,27 @@ function changeZIndex(){
     }
 }
 
+function zoomPicture(){
+    let zoom =  document.getElementById("bigger");
+    let wrapper =  document.getElementById("wrapper");
+    let bluredArea = document.querySelector("body:not(#bigger)");
+    if(zoom.getAttribute('state') && zoom.getAttribute('state') === 'zoomed'){
+        zoom.style.transform = 'none';
+        zoom.style.position = "static";
+        zoom.setAttribute('state', 'not-zoomed');
+        bluredArea.style.filter = "blur(0)";
+    }else{
+        zoom.style.transform = "scale(2) translate(22.5%,-45%)";
+        zoom.style.position = "fixed";
+        zoom.setAttribute('state', 'zoomed');
+        // bluredArea.style.filter = "blur(5rem)";
+/*         wrapper.style.height = "100vh";
+        wrapper.style.background = "rgba(0,0,0,.8)";
+        wrapper.style.position ="absolute";
+        wrapper.style.zIndex ="100"; */
+    }
 
+}
 
 document.querySelector('#checkbox').addEventListener('click', changeZIndex);
+document.querySelector('#bigger').addEventListener('click', zoomPicture);
